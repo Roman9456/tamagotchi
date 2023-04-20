0967603b96f7f00e8b78a182dcef01caa26ea4c3
CaVaIr <Unlimi.net@mail.ru>
2023-04-20 23:04:43 +0300

    test randome time

--- tamagochi.py
+++ tamagochi.py
@@ -1,78 +1,90 @@
 import random
-import schedule # Модуль планировщик
+import schedule  # Модуль планировщик
 import time
 
-# def randome_status():
+def rand_func():
+    '''
+    Выполняет функцию в случайном диапозоне времени от 1 сек. до 10 сек.
+    '''
+    print("Хочу есть ... ")
 
 
-class Tamagotchi:
-    def __init__(self, name):
-        self.name = name
-        self.hunger = 50
-        self.health = 50
-        self.happiness = 50
+schedule.every(3).to(10).seconds.do(rand_func)
 
-    def feed(self):
-        self.hunger -= 10
-        self.happiness += 10
-        self.check_limits()
+while True:
+    schedule.run_pending()
 
-    def heal(self):
-        self.health += 10
-        self.happiness += 5
-        self.check_limits()
 
-    def play(self):
-        self.hunger += 10
-        self.happiness += 20
-        self.check_limits()
 
-    def check_limits(self):
-        if self.hunger > 100:
-            self.hunger = 100
-        elif self.hunger < 0:
-            self.hunger = 0
 
-        if self.health > 100:
-            self.health = 100
-        elif self.health < 0:
-            self.health = 0
+# class Tamagotchi:
+#     def __init__(self, name):
+#         self.name = name
+#         self.hunger = 50
+#         self.health = 50
+#         self.happiness = 50
 
-        if self.happiness > 100:
-            self.happiness = 100
-        elif self.happiness < 0:
-            self.happiness = 0
+#     def feed(self):
+#         self.hunger -= 10
+#         self.happiness += 10
+#         self.check_limits()
 
-    def status(self):
-        print(f"Имя: {self.name}")
-        print(f"Голод: {self.hunger}")
-        print(f"Здоровье: {self.health}")
-        print(f"Счастье: {self.happiness}")
+#     def heal(self):
+#         self.health += 10
+#         self.happiness += 5
+#         self.check_limits()
 
-def main():
-    name = input("Введите имя тамагочи: ")
-    tamagotchi = Tamagotchi(name)
+#     def play(self):
+#         self.hunger += 10
+#         self.happiness += 20
+#         self.check_limits()
 
-    while True:
-        tamagotchi.status()
+#     def check_limits(self):
+#         if self.hunger > 100:
+#             self.hunger = 100
+#         elif self.hunger < 0:
+#             self.hunger = 0
 
-        action = input("Что вы хотите сделать? (кормить, лечить, играть): ")
+#         if self.health > 100:
+#             self.health = 100
+#         elif self.health < 0:
+#             self.health = 0
 
-        if action == "кормить":
-            tamagotchi.feed()
-        elif action == "лечить":
-            tamagotchi.heal()
-        elif action == "играть":
-            tamagotchi.play()
-        else:
-            print("Неизвестное действие")
+#         if self.happiness > 100:
+#             self.happiness = 100
+#         elif self.happiness < 0:
+#             self.happiness = 0
 
-        if tamagotchi.happiness <= 0:
-            print("Тамагочи ушел в депрессию и умер...")
-            break
-        elif tamagotchi.hunger >= 100 or tamagotchi.health <= 0:
-            print("Тамагочи умер от голода или болезни...")
-            break
+#     def status(self):
+#         print(f"Имя: {self.name}")
+#         print(f"Голод: {self.hunger}")
+#         print(f"Здоровье: {self.health}")
+#         print(f"Счастье: {self.happiness}")
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     name = input("Введите имя тамагочи: ")
+#     tamagotchi = Tamagotchi(name)
+
+#     while True:
+#         tamagotchi.status()
+
+#         action = input("Что вы хотите сделать? (кормить, лечить, играть): ")
+
+#         if action == "кормить":
+#             tamagotchi.feed()
+#         elif action == "лечить":
+#             tamagotchi.heal()
+#         elif action == "играть":
+#             tamagotchi.play()
+#         else:
+#             print("Неизвестное действие")
+
+#         if tamagotchi.happiness <= 0:
+#             print("Тамагочи ушел в депрессию и умер...")
+#             break
+#         elif tamagotchi.hunger >= 100 or tamagotchi.health <= 0:
+#             print("Тамагочи умер от голода или болезни...")
+#             break
+
+# if __name__ == "__main__":
+#     main()
