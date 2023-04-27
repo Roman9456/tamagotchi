eca614a08ef35ab3199176d294b6a99a1ec81f39
Roman Smolyakov <123468411+Roman9456@users.noreply.github.com>
2023-04-27 16:01:29 +0400

    Fix

--- tamagochi.py
+++ tamagochi.py
@@ -1,3 +1,74 @@
+  class Tamagotchi:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 50
+        self.health = 50
+        self.happiness = 50
+
+    def feed(self):
+        self.hunger -= 10
+        self.happiness += 10
+        self.check_limits()
+
+    def heal(self):
+        self.health += 10
+        self.happiness += 5
+        self.check_limits()
+
+    def play(self):
+        self.hunger += 10
+        self.happiness += 20
+        self.check_limits()
+
+    def check_limits(self):
+        if self.hunger > 100:
+            self.hunger = 100
+        elif self.hunger < 0:
+            self.hunger = 0
+
+        if self.health > 100:
+            self.health = 100
+        elif self.health < 0:
+            self.health = 0
+
+        if self.happiness > 100:
+            self.happiness = 100
+        elif self.happiness < 0:
+            self.happiness = 0
+
+    def status(self):
+        print(f"Имя: {self.name}")
+        print(f"Голод: {self.hunger}")
+        print(f"Здоровье: {self.health}")
+        print(f"Счастье: {self.happiness}")
+
+
+class Cat(Tamagotchi):
+    def __init__(self, name):
+        super().__init__(name)
+        self.sound = "Мяу"
+        self.species = "Кошка"
+
+    def make_sound(self):
+        print(self.sound)
+
+
+class Dog(Tamagotchi):
+    def __init__(self, name):
+        super().__init__(name)
+        self.sound = "Гав"
+        self.species = "Собака"
+
+    def make_sound(self):
+        print(self.sound)
+
+
+class Parrot(Tamagotchi):
+    def __init__(self, name):
+        super().__init__(name)
+        self.sound = "Кар-кар"
+        self.species = "Попугай"
+
     def make_sound(self):
         print(self.sound)
 
